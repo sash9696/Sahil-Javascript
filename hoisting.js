f5cfb1eb8f7c05ce81f3ce2a42da730345e67c25
sash9696 <67851308+sash9696@users.noreply.github.com>
2021-11-28 22:57:55 +0530

    Add files via upload

---
+++ hoisting.js
@@ -0,0 +1,17 @@
+// getName();
+// console.log(x);
+console.log(getName);
+console.log(getName2);
+var x = 10;
+// function getName(){
+//     console.log("Sash rulz");
+// }
+var getName2 = function(){
+
+}
+var getName = () => {
+    console.log("Sash rulz");
+}
+// console.log(getName);
+// getName();
+// console.log(x);
