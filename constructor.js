41120543c61ca47d52e77d97dd383fa3b63e72b4
sash9696 <67851308+sash9696@users.noreply.github.com>
2021-12-08 22:52:38 +0530

    Add files via upload

---
+++ constructor.js
@@ -0,0 +1,8 @@
+class Employee {  
+    constructor() {  
+      this.id=101;  
+      this.name = "Martin Roy";  
+    }   
+  }  
+  var emp = new Employee();  
+  document.writeln(emp.id+" "+emp.name);  
