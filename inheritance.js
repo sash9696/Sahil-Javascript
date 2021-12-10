08446392ac086df43a1a4d32f843dcf10d44cb51
sash9696 <67851308+sash9696@users.noreply.github.com>
2021-12-10 21:56:22 +0530

    Add files via upload

---
+++ inheritance.js
@@ -0,0 +1,7 @@
+class Moment extends Date {  
+    constructor() {  
+      super();  
+    }}  
+  var m=new Moment();  
+  document.writeln("Current date:")  
+  document.writeln(m.getDate()+"-"+(m.getMonth()+1)+"-"+m.getFullYear());  
